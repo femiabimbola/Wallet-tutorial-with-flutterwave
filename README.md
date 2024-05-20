779f7d8d20249d86ff6dc0850a8fb9ea48728c00
Femi Abimbola <femiabimbola6@gmail.com>
2024-05-20 12:05:31 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# wallet-demo-with-flutterwave
+# A wallet creation with Flutterwave
+
 This is a Demo on how to build a wallet system with flutterwave payment integration
--- config/database.js
+++ config/database.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose");
 
-const { MONGO_URI } = process.env;
+const dotenv = require('dotenv')
 
-exports.connect = () => {
-  // Connecting to the database
-  mongoose
-    .connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Successfully connected to database");
-    })
-    .catch((error) => {
-      console.log("database connection failed. exiting now...");
-      console.error(error);
-      process.exit(1);
+// dotenv.config();
+const connectionString = process.env.DB_STRING 
+
+exports.connect = async () => {
+  try {
+    await mongoose.connect(connectionString, {
+      autoIndex: true, 
     });
+    console.log("Connected to Mongodb Atlas");
+  } catch (error) {
+    console.log("database connection failed. exiting now...");
+    console.error(error);
+    process.exit(1);
+  }
 };
